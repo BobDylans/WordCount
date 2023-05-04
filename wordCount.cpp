8e0bec0b2dc5d27fb2207e340b77f1d7e48a99b7
BobDylans <2603232854@qq.com>
2023-05-04 19:19:07 +0800

    first

--- wordCount.cpp
+++ wordCount.cpp
@@ -1,41 +1,35 @@
-#include "stdio.h"
-
-#define IN  1  /* inside a word */
-
-#define OUT 0  /* outside a word */
-
-int  main()
-
+#include <stdio.h>
+#include <string.h>
+int main (int argc, char *argv[])
 {
-
-int c, nl, nw, nc, state;
-
-state = OUT;
-
-nl = nw = nc = 0;
-
-while ((c = getchar()) != EOF) {
-
-++nc;  //characters
-
-if (c == '\n')
-
-++nl; //rows
-
-if (c == ' ' || c == '\n' || c == '\t')
-
-state = OUT;
-
-else if (state == OUT) {
-
-state = IN;
-
-++nw; //words
-
+    // argc 表示参数个数, *argv[]指针数组, 每一个指针指向一个字符串
+    FILE *fp;
+    int cnt = 0; // 统计数
+    if ((fp=fopen(argv[2], "r")) == NULL)
+    {
+        puts("error!\n");
+        return 0;
+    }
+    if (argv[1][1] == 'w') // 统计单词
+    {
+        char s[1024];
+        while (fscanf(fp, "%s", s) != EOF)
+        {
+            cnt ++;
+            for (int i = 1; i < strlen(s) - 1; i ++)
+                if (s[i] == ',' && s[i - 1] != ',' && s[i + 1] != ',') 
+                    cnt ++;
+        }
+        printf("单词数=%d\n", cnt);
+    }
+    else if (argv[1][1] == 'c') // 统计字符
+    {
+        char c;
+        while ((c = fgetc(fp)) != EOF) cnt ++;
+        printf("字符数=%d", cnt);
+    }
+    fclose(fp);
+    return 0;
 }
 
-}
 
-printf("%d %d %d\n", nl, nw, nc);
-
-}
